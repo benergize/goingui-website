5a5d5744277aa25ce09e582c36efd8fa17cce782
Benjamin Ehrlich <bensaysstuff@gmail.com>
2020-03-29 18:40:28 -0400

    Create light-select.js

---
+++ components/light-select.js
@@ -0,0 +1,8 @@
+app.register("light-select", {
+  "html": `
+    <select class = 'jbind' data-jname = 'bgc'>
+      <option value = "white">Less Dim</option>
+      <option value = "#e8e8e8">Dimmer</option>
+    </select>
+  `
+});
